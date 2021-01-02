7737c0d66f4ff3634f6853e4bda02ca307395f3c
MrZ626 <1046101471@qq.com>
2021-01-02 20:46:59 +0800

    准备好葡萄牙语文件等待翻译

--- parts/scenes/lang.lua
+++ parts/scenes/lang.lua
@@ -11,8 +11,9 @@ scene.widgetList={
 	WIDGET.newButton{name="en",		x=640,	y=100,w=200,h=120,font=45,code=setLang(3)},
 	WIDGET.newButton{name="fr",		x=860,	y=100,w=200,h=120,font=45,code=setLang(4)},
 	WIDGET.newButton{name="sp",		x=1080,	y=100,w=200,h=120,font=45,code=setLang(5)},
-	WIDGET.newButton{name="symbol",	x=200,	y=250,w=200,h=120,font=45,code=setLang(6)},
-	WIDGET.newButton{name="yygq",	x=420,	y=250,w=200,h=120,font=45,code=setLang(7)},
+	WIDGET.newButton{name="pt",		x=200,	y=250,w=200,h=120,font=45,code=setLang(6)},
+	WIDGET.newButton{name="symbol",	x=420,	y=250,w=200,h=120,font=45,code=setLang(7)},
+	WIDGET.newButton{name="yygq",	x=640,	y=250,w=200,h=120,font=45,code=setLang(8)},
 	WIDGET.newButton{name="back",	x=640,	y=600,w=200,h=80,font=35,code=WIDGET.lnk_BACK},
 }
 
